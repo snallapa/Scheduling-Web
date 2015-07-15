9be817b2800edeba38deb0574f9fd8bf9359c3b9
Sahith Nallapareddy <sahith.reddy@gmail.com>
2015-07-15 18:06:38 -0400

    protecting release code

--- WebContent/js/parse.js
+++ WebContent/js/parse.js
@@ -2,5 +2,4 @@
 /**
 *
 */
-Parse.initialize("2LtDjadP74Th3XfebZvQjEtjvK9wqkKNsG8XOpbq",
-	"x2LKddxBl61XGECpX2v1LbIpst21uNvrFN527rPN");
+Parse.initialize("p5ciZMOdv5INvdViiYMGoQOf4FaHMOKJ6pRQWAkI", "zuM1Ddqgut5QUKitEdEpIFgCbqBpkhHMPfyhuQmA");
